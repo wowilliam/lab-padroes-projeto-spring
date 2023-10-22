6e85f2f69829c9bc457a263146ba55c1b37c2520
Your Name <youremail@example.com>
2023-10-22 12:34:49 -0300

    Atualizando

--- src/main/java/one/digitalinnovation/gof/controller/ClienteRestController.java
+++ src/main/java/one/digitalinnovation/gof/controller/ClienteRestController.java
@@ -18,8 +18,6 @@ import one.digitalinnovation.gof.service.ClienteService;
  * Esse {@link RestController} representa nossa <b>Facade</b>, pois abstrai toda
  * a complexidade de integrações (Banco de Dados H2 e API do ViaCEP) em uma
  * interface simples e coesa (API REST).
- * 
- * @author falvojr
  */
 @RestController
 @RequestMapping("clientes")
